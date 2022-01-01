e6996026889767d0d4461d313f3a044cbac7f641
Alex Kokobane <alexkokobane@gmail.com>
2022-01-01 20:08:50 +0200

    partially done

--- Program.cs
+++ Program.cs
@@ -5,11 +5,31 @@ Random num = new Random();
 
 int cpuNum = num.Next(15);
 
-Console.WriteLine("Guess a number between 0 and 15:");
-int userNum = Int32.Parse(Console.ReadLine());
 
-if(userNum != cpuNum){
-	Console.WriteLine("Wrong! The right answer is "+cpuNum);
-} else if(userNum == cpuNum){
-	Console.WriteLine("Good guess!");
+
+int hearts = 15;
+while(hearts > 0 && hearts < 30) {
+	Console.WriteLine("Guess a number between 0 and 15:");
+	int userNum = Int32.Parse(Console.ReadLine());
+	if(userNum != cpuNum){
+		hearts--;
+		Console.WriteLine($"Wrong! The right answer is {cpuNum}");
+		Console.WriteLine();
+		Console.WriteLine($"You have {hearts} points");
+		Console.WriteLine("**************************");
+		Console.WriteLine();
+	} else if(userNum == cpuNum){
+		Console.WriteLine("Good guess!");
+		hearts++;
+		Console.WriteLine($"You have {hearts} points");
+		Console.WriteLine();
+		Console.WriteLine("**************************");
+		Console.WriteLine();
+	}
+};
+
+if(hearts == 0){
+	Console.WriteLine("You lose!");
+} else {
+	Console.WriteLine("You win!");
 }
