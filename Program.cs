2007ada29c39b06f5e5e3d3c5d7e5f6e7938089a
Alex Kokobane <alexkokobane@gmail.com>
2022-01-02 22:05:57 +0200

    problem with the increment/decrement to be fixed

--- Program.cs
+++ Program.cs
@@ -4,37 +4,49 @@
 int hearts = 15;
 while(hearts > 0 && hearts < 30) {
 	Random num = new Random();
-	int cpuNum = num.Next(15);
-	int defNum1 = num.Next(15);
-	int defNum2 = num.Next(15);
-	int[] guess = {cpuNum, defNum1, defNum2};
-	Console.WriteLine("Guess the correct number from these three");
+	int randomPoints = num.Next(0,6);
+	int cpuNum = num.Next(50);
+	int defNum1 = num.Next(50);
+	int defNum2 = num.Next(50);
+	int defNum3 = num.Next(50);
+	int defNum4 = num.Next(50);
+	int defNum5 = num.Next(50);
+	int[] guess = {cpuNum, defNum1, defNum2, defNum3, defNum4, defNum5};
+	guess = guess.OrderBy(arr => num.Next()).ToArray();
+	Console.WriteLine(">>Guess the correct number from these three!!");
 	foreach(int n in guess){
-		Console.WriteLine(n);
+		Console.Write(n+" ");
 	}
-	string reader = Console.ReadLine();
-	int userNum;
-	if(Int32.TryParse(reader, out userNum)) return userNum;
+	Console.WriteLine();
+	Console.Write("Ans: ");
+	int userNum = Int32.Parse(Console.ReadLine());
+	System.Threading.Thread.Sleep(1000);
 	if(userNum != cpuNum){
-		hearts--;
+		hearts-randomPoints;
 		Console.WriteLine();
 		Console.WriteLine($"Wrong! The right answer is {cpuNum}");
 		Console.WriteLine();
 		Console.WriteLine($"You have {hearts} points");
 		Console.WriteLine("**************************");
 		Console.WriteLine();
+		System.Threading.Thread.Sleep(1000);
+		Console.Write("Press enter :(");
+		Console.ReadKey();
 	} else if(userNum == cpuNum){
-		hearts++;
+		hearts+randomPoints;
 		Console.WriteLine();
 		Console.WriteLine("Good guess!");
 		Console.WriteLine();
 		Console.WriteLine($"You have {hearts} points");
 		Console.WriteLine("**************************");
 		Console.WriteLine();
+		System.Threading.Thread.Sleep(1000);
+		Console.Write("Enter any key :)");
+		Console.ReadKey();
 	}
 };
 
-if(hearts == 0){
+if(hearts <= 0){
 	Console.WriteLine("You lose!");
 } else {
 	Console.WriteLine("You win!");
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Guesswhat?
 
 **A console application built with C#**
+
+I am building this project as a way to learn C#. No specific tutorial was followed in the making, I am  figuring things out as I go.
